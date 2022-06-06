bfbceca9a99409a0e6c7de438f583ced117274d3
WissemBT <wissembettaieb2000@gmail.com>
2022-06-06 10:58:27 +0100

    commit third

--- helloworld.js
+++ helloworld.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('hello world');
+  res.end('hello everyone');
 });
 
 server.listen(port, hostname, () => {
